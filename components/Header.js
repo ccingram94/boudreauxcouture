6010b673097cf986540c73ec809441a7c6b94b1d
Constancegit status <constanceingram94@gmail.com>
2022-06-11 16:58:20 -0500

    added some hero and header styling

--- components/Header.js
+++ components/Header.js
@@ -3,7 +3,7 @@ import { ShoppingCartIcon, UserIcon } from '@heroicons/react/outline'
 export default function Header () {
     return (
         <div className="flex flex-row w-screen text-white bg-bluebonnets bg-cover bg-bottom">
-            <div className="flex flex-row flex-wrap justify-between justify-center items-center w-full bg-blue-900 bg-opacity-80 ">
+            <div className="flex flex-row flex-wrap justify-between justify-center items-center w-full bg-blue-900 bg-opacity-80 drop-shadow-xl">
                 <h1 className="font-cinzel text-lg md:text-3xl p-6">☙ Boudreaux ❧</h1>
                 <div className="flex flex-row flex-wrap p-3 m-3 text-md md:text-3xl">
                     <ShoppingCartIcon className="h-12 w-12 md:text-xl p-3" />
---
+++ components/Subheader.js
@@ -0,0 +1,7 @@
+export default function Subheader () {
+    return (
+        <div className="flex flex-row w-screen text-white bg-gray-900 font-cinzel text-sm md:text-md text-center p-3">
+            <h1>   </h1>
+        </div>
+    )
+}
