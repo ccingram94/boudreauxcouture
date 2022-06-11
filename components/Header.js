a5800de7602d368a96c9c7821026fa4fbb289459
Constancegit status <constanceingram94@gmail.com>
2022-06-11 13:54:21 -0500

    added some templates for product api, header, and footer components

---
+++ components/Header.js
@@ -0,0 +1,14 @@
+
+export default function Header () {
+    return (
+        <div className="flex flex-row flex-wrap">
+            <h1 className="font-italiana text-lg md:text-3xl p-6">☙ Boudreaux Couture ❧</h1>
+            <div className="flex flex-row flex-wrap p-3">
+                <h1 className="font-italiana text-md md:text-xl p-3">Option 1</h1>
+                <h1 className="font-italiana text-md md:text-xl p-3">Option 2</h1>
+                <h1 className="font-italiana text-md md:text-xl p-3">Option 3</h1>
+            </div>
+            <div className="flex flex-row flex-wrap p-3"></div>
+        </div>
+    )
+}
