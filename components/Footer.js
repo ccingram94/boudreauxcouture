6790b39eac6a825991b2a8bc46ef5515178f530a
Constancegit status <constanceingram94@gmail.com>
2022-06-11 16:33:45 -0500

    fixing the spacing on header

--- components/Footer.js
+++ components/Footer.js
@@ -3,7 +3,7 @@ export default function Footer () {
     return (
         <div className="flex flex-col text-center text-white w-screen bg-bluebonnets bg-cover bg-bottom">
             <div className="flex flex-col w-full bg-blue-900 bg-opacity-80">
-                <h1 className="font-cinzel text-lg md:text-3xl p-6">☙ Boudreaux Couture ❧</h1>
+                <h1 className="font-cinzel text-lg md:text-3xl p-6">☙ Boudreaux ❧</h1>
                 <h2 className="font-cinzel text-md md:text-lg">jewels from the Heart of Texas</h2>
                 <h1 className="font-italiana text-lg md:text-3xl p-6">❦</h1>
             </div>
--- components/Header.js
+++ components/Header.js
@@ -2,10 +2,10 @@ import { ShoppingCartIcon, UserIcon } from '@heroicons/react/outline'
 
 export default function Header () {
     return (
-        <div className="flex flex-row flex-wrap w-screen text-white bg-bluebonnets bg-cover bg-bottom">
+        <div className="flex flex-row w-screen text-white bg-bluebonnets bg-cover bg-bottom">
             <div className="flex flex-row flex-wrap justify-between justify-center items-center w-full bg-blue-900 bg-opacity-80 ">
-                <h1 className="font-cinzel text-lg md:text-3xl p-6">☙ Boudreaux Couture ❧</h1>
-                <div className="flex flex-row flex-wrap p-12 m-3 text-md md:text-3xl">
+                <h1 className="font-cinzel text-lg md:text-3xl p-6">☙ Boudreaux ❧</h1>
+                <div className="flex flex-row flex-wrap p-3 m-3 text-md md:text-3xl">
                     <ShoppingCartIcon className="h-12 w-12 md:text-xl p-3" />
                     <UserIcon className="h-12 2-12 md:text-xl p-3" />
                 </div>
