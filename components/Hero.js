4ca52c37dbd666378270cbd13b8bc68a7ba4b081
Constancegit status <constanceingram94@gmail.com>
2022-06-16 22:43:05 -0500

    adding hero section for jewelry stock

--- components/Hero.js
+++ components/Hero.js
@@ -1,19 +1,28 @@
 import Link from 'next/link'
+import Image from 'next/image'
+import austin from '../public/austin.jpg'
 
 
 export default function Hero () {
     return (
         <div className="w-screen h-full flex flex-row flex-wrap text-center justify-center">
             <div className="w-full xl:w-1/2 h-full">
-                <div className="w-full bg-gradient-to-l from-blue-100 to-blue-300">
-                    <video autoPlay loop muted className="w-full opacity-90">
-                        <source src="brides.mp4" type="video/mp4" />
-                    </video>
+                <div className="w-full bg-blue-900">
+                    <div className="w-full bg-transparent-to-b bg-blue-300">
+                        <Image src={austin} className="opacity-50" />
+                    </div>
                 </div>
             </div>
             <div className="bg-gradient-to-t from-indigo-100 to-gray-100 w-screen xl:w-1/2 text-blue-900 font-cinzel text-center p-12">
-                <h1 className="text-2xl md:text-3xl pt-6 lg:pt-12">as unique as the bride</h1>
-                <h2 className="text-md md:text-xl p-3 m-3">engagement rings ❦ wedding bands ❦ bridal jewelry</h2>
+                <h1 className="text-2xl md:text-3xl pt-6 lg:pt-12">The Austin Collection</h1>
+                <h2 className="text-md md:text-lg p-3 m-3"> Freshwater Pearls ❦ Gold Tones ❦ Amethyst</h2>
+                <div className="flex flex-row flex-wrap justify-center text-center p-3 m-6">
+                    <div className="p-3">
+                        <h2 className="text-md lg:text-xl border-b-2 p-2">The Violet Crown I and II</h2>
+                        <h3 className="text-sm lg:text-md p-2">Amethyst ❦ Freshwater Pearls</h3>
+                        <p className="text-sm lg:text-md font-roboto">Violet crowns of amethyst accented by genuine freshwater pearls and gold plated brass.</p>
+                    </div>
+                </div>
             </div>
         </div>
     )
